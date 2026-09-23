037ce82ce4e10d65dbd05d62d04ba179932d2056
m=0 <54184361+ya1-oa@users.noreply.github.com>
2026-09-23 12:23:50 -0700

    Bust cache for stable single Home map

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courier-radar-v8';
+const CACHE='courier-radar-v9';
 const STATIC=['/','/styles.css','/app.js','/map.js','/lib-network.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{

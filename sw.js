1aacd0779716903e743d1231f89edf75ac339e57
m=0 <54184361+ya1-oa@users.noreply.github.com>
2026-09-23 20:20:57 -0700

    Ship pre-shift intelligence planner

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courier-radar-v17';
+const CACHE='courier-radar-v18';
 const STATIC=['/','/styles.css','/app.js','/map.js','/lib-network.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{

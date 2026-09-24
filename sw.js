a601e55b8ef9eb6c51f81e48cfd396ee2d9df81c
m=0 <54184361+ya1-oa@users.noreply.github.com>
2026-09-24 02:51:23 -0700

    Ship end-of-shift recap v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courier-radar-v19';
+const CACHE='courier-radar-v20';
 const STATIC=['/','/styles.css','/app.js','/map.js','/lib-network.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{

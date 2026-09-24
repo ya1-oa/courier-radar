b51c7e0671232fd92e92cd2efe39bb51aa98276d
m=0 <54184361+ya1-oa@users.noreply.github.com>
2026-09-23 20:33:44 -0700

    Ship Hobby-compatible Radar deployment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courier-radar-v18';
+const CACHE='courier-radar-v19';
 const STATIC=['/','/styles.css','/app.js','/map.js','/lib-network.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{

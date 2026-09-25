599cb2f615e46350ce1a34f39cc7c6e9e55cbd8f
m=0 <54184361+ya1-oa@users.noreply.github.com>
2026-09-25 10:08:36 -0700

    Ship event-aware planner v22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courier-radar-v21';
+const CACHE='courier-radar-v22';
 const STATIC=['/','/styles.css','/app.js','/map.js','/lib-network.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{

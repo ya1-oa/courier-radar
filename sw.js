edb98ec380ef1bd0456d1bbaf2b7a26ac7d99e94
m=0 <54184361+ya1-oa@users.noreply.github.com>
2026-09-25 19:12:20 -0700

    Ship exploration and post-drop UI v24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courier-radar-v23';
+const CACHE='courier-radar-v24';
 const STATIC=['/','/styles.css','/app.js','/map.js','/lib-network.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{

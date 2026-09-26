2221f4ec554381b25c5688fd28200845193902c9
m=0 <54184361+ya1-oa@users.noreply.github.com>
2026-09-25 20:34:27 -0700

    Ship clean map module and consolidated Home v28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='courier-radar-v27';
+const CACHE='courier-radar-v28';
 const STATIC=['/','/styles.css','/app.js','/map.js','/lib-network.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{
